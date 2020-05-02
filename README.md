74e20ac726968ca21d49d6c73e03abdbdd012d62
Igor Kurek <igorkurek96@gmail.com>
2020-05-03 01:02:10 +0200

    Added MNIST dataset

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 ## Dataset Sources
 * [Kaggle Cats vs Dogs](https://www.kaggle.com/c/dogs-vs-cats)
 * [Classic CIFAR10 as raw images](https://github.com/YoongiKim/CIFAR-10-images)
+* [MNIST as raw images](https://github.com/myleott/mnist_png)
 
 ## Notebooks
 * [LeNet-5](/notebooks/lenet5)
   * [Dogs vs Cats](/notebooks/lenet5/lenet5_catvsdog.ipynb)
   * [CIFAR10](/notebooks/lenet5/lenet5_cifar10.ipynb)
+  * [MNIST](/notebooks/lenet5/lenet5_mnist.ipynb)
   
 ## Models
 * [LeNet-5](/src/keras_model/lenet5_keras_model.py)
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from src.datasets.dataset_generators import DatasetGenerators
+from src.preprocessing.mnist_splitter import MnistSplitter
+from keras.preprocessing.image import ImageDataGenerator
 
-data_generators = DatasetGenerators(name='cifar10', image_size=(32, 32))
+#MnistSplitter().split()
+
+trdata = ImageDataGenerator()
+traindata = trdata.flow_from_directory(
+    directory="/Users/igor/Python/CNN-Architecture-Comparison/data/mnist/train/", target_size=(224, 224))
+tsdata = ImageDataGenerator()
+testdata = tsdata.flow_from_directory(directory="/Users/igor/Python/CNN-Architecture-Comparison/data/mnist/test/",
+                                      target_size=(224, 224))
+vddata = ImageDataGenerator()
+validationdata = vddata.flow_from_directory(
+    directory="/Users/igor/Python/CNN-Architecture-Comparison/data/mnist/validate/", target_size=(224, 224))
