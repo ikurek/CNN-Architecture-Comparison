1d477b02881a9963c6c4629df643e163f0112373
Igor Kurek <igorkurek96@gmail.com>
2020-05-10 22:21:16 +0200

    Updated even more notebooks

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-from keras.preprocessing.image import ImageDataGenerator
+from src.preprocessing.extract_part_of_dataset import DatasetPartExtractor
 
-# DatasetPartExtractor().extract('caltech101', 30)
-
-datasets = ['caltech101', 'cat-vs-dog_10', 'cifar10', 'cifar10_10', 'mnist', 'mnist_10']
-
-for dataset in datasets:
-    print(dataset)
-    trdata = ImageDataGenerator()
-    traindata = trdata.flow_from_directory(
-        directory="/Users/igor/Python/CNN-Architecture-Comparison/data/{}/train/".format(dataset),
-        target_size=(224, 224))
-    tsdata = ImageDataGenerator()
-    testdata = tsdata.flow_from_directory(
-        directory="/Users/igor/Python/CNN-Architecture-Comparison/data/{}/test/".format(dataset),
-        target_size=(224, 224))
-    vddata = ImageDataGenerator()
-    validationdata = vddata.flow_from_directory(
-        directory="/Users/igor/Python/CNN-Architecture-Comparison/data/{}/validate/".format(dataset),
-        target_size=(224, 224))
+DatasetPartExtractor().extract('caltech101', 40)
+DatasetPartExtractor().extract('cat-vs-dog', 10)
+DatasetPartExtractor().extract('cifar10', 5)
+DatasetPartExtractor().extract('mnist', 5)
