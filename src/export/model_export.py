afe37508b40e460cdce70b36419af6bd45b8dba5
Igor Kurek <igorkurek96@gmail.com>
2020-05-19 22:01:42 +0200

    Finished LeNet-5 notebooks

--- src/export/model_export.py
+++ src/export/model_export.py
@@ -3,5 +3,8 @@ import os
 
 
 def clear_exported_models(model_directory):
+    model_dir = os.path.dirname(model_directory)
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
     for file in glob.glob(f'{model_directory}*'):
         os.remove(file)
